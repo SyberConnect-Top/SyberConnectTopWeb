22a586ac500f22b89f2bbb33a9602f5729fa87d4
sofiaparson1973 <sofia.parson.73@mail.ru>
2022-04-08 12:00:43 +0800

    deploy web : V0.01:update

---
+++ assets/welcome.html.c39aa0d7.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to SyberConnect.Top",lang:"en-US",frontmatter:{index:1,title:"Welcome to SyberConnect.Top",author:"SyberConnect.Top",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`SyberConnect Top : Unified login.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://syberconnect.top/welcome.html"}],["meta",{property:"og:title",content:"Welcome to SyberConnect.Top"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"SyberConnect.Top"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>SyberConnect Top : Unified login.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"SyberConnect Top",slug:"syberconnect-top",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why SyberConnectTop?",slug:"why-syberconnecttop",children:[]},{level:2,title:"Tech Stack to Build SyberConnect Top",slug:"tech-stack-to-build-syberconnect-top",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649389946e3,updatedTime:1649389946e3,contributors:[{name:"sofiaparson1973",email:"sofia.parson.73@mail.ru",commits:1}]},readingTime:{minutes:.44,words:132},filePathRelative:"welcome.md"};export{e as data};
